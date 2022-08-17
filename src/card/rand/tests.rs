4abc6eb385f3ec2b60839a8dc58c90f2afe94700
Nabla <any.nabla@gmail.com>
2022-08-18 00:11:04 +0900

    impl Distribution<Card> for Standard

---
+++ src/card/rand/tests.rs
@@ -0,0 +1,30 @@
+use super::Card;
+use rand::{
+    thread_rng,
+    Rng as _,
+};
+
+/// Tests whether cards are generated correctly.
+#[test]
+fn test() {
+    let mut rng = thread_rng();
+
+    for _ in 0..0xFF {
+        for (column, numbers) in rng.gen::<Card>().numbers.chunks_exact(5).enumerate() {
+            let base = 15 * column;
+            let mut generated = [false; 15];
+
+            for &number in numbers {
+                match number {
+                    0 => (),
+                    _ => {
+                        let number = (number - 1) as usize;
+                        assert!((base..base + 15).contains(&number));
+                        assert!(!generated[number - base]);
+                        generated[number - base] = true;
+                    },
+                }
+            }
+        }
+    }
+}
